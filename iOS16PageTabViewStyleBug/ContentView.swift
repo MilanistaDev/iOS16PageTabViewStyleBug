9ecd69737779163820d0ecedd6445707e93357cd
Takuya Aso <milanista224@gmail.com>
2024-07-24 16:38:48 +0900

    Initial Commit

---
+++ iOS16PageTabViewStyleBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS16PageTabViewStyleBug
+//
+//  Created by Takuya Aso on 2024/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS16PageTabViewStyleBug/iOS16PageTabViewStyleBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16PageTabViewStyleBugApp.swift
+//  iOS16PageTabViewStyleBug
+//
+//  Created by Takuya Aso on 2024/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS16PageTabViewStyleBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
